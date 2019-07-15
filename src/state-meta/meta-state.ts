a814755c46608ba91d87eedfb345b9c9c2a9c9e4
komei-work-work <koba.work,work1127@gmail.com>
2019-07-15 23:31:58 +0900

    [feature/nest] meta start type

--- src/state-meta/meta-state.ts
+++ src/state-meta/meta-state.ts
@@ -8,7 +8,7 @@ export const AnytimeName = '*';
 // tslint:disable-next-line:max-classes-per-file
 class StringState implements StateType<undefined, string> {
     public readonly name: string = StartName;
-    public getState(): void {
+    public getState(): undefined {
         return undefined;
     }
     public toString(): string {
