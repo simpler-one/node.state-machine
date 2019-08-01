ce31c545c611b82dc73b84fc0b2281ea8a0e668c
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-08-01 16:57:13 +0900

    Update transitions.ts

--- src/statechart-writer/puml-writer/transitions.ts
+++ src/statechart-writer/puml-writer/transitions.ts
@@ -3,20 +3,32 @@ import { Transition } from './transition';
 
 export class Transitions {
 
-    private readonly all: Map<string, Transition> = Map<string, Transition>();
-    private readonly bundle: Map<string, Transition[]> = Map<string, Transition[]>();
+    private readonly map: Map<string, Transition> = Map<string, Transition>();
     
     public add(transition: Transition): void {
-        const prevTr = this.all.get(path);
+        const prevTr = this.map.get(path);
         const newTr = Transition.join(prevTr, new Transition(this.count, from, to, act));
-        this.all.set(path, newTr);
+        this.map.set(path, newTr);
+    }
+    
+    public bundleSameDestination(from: string): Transitions[] {
+        const bundler = new Map<string, Transition[]>();
+        this.map.forEach(tr => {
+            const bundled = bundler.get(tr.from);
+            if (bundled) {
+                bundled = [];
+                bundler.set(tr.from, bundled);
+            }
+            
+            bundled.push(tr);
+        });
 
-        let bundled = this.bundle.get(tr.to);
-        if (!bundled) {
-            bundled = [];
-            this.bundle.set(tr.to, bundled);
-        }
-        
-        bundled.push(tr);
+        const result: Transition[] = [];
+        bundler.forEach(transitions => {
+            transitions.sort(Transition.compare);
+            result.push(Transition.join(transitions));
+        });
+
+        return result;
     }
 }
