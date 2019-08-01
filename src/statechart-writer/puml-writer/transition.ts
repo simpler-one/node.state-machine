688e225ec02b02bb88da19a6fb9d9cdfb8ff76b0
work-work-komei <40133262+work-work-komei@users.noreply.github.com>
2019-08-01 14:41:06 +0900

    Update transition.ts

--- src/statechart-writer/puml-writer/transition.ts
+++ src/statechart-writer/puml-writer/transition.ts
@@ -1,9 +1,18 @@
 export class Transition {
-    constructor(
+    public constructor(
         public readonly order: number,
         public readonly from: string,
         public readonly to: string,
         public readonly action: string,
     ) {
+    }
+
+    public appendAction(action: string): Transition {
+        return new Transition(
+            this.order,
+            this.from,
+            this.to,
+            this.action + ',' + action,
+        );
     }
 }
