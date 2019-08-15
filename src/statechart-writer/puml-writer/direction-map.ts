0f2174300bf99443aea635259f85edbc085b84bf
komei-work-work <koba.work,work1127@gmail.com>
2019-08-15 19:03:05 +0900

    [puml] lint

--- src/statechart-writer/puml-writer/direction-map.ts
+++ src/statechart-writer/puml-writer/direction-map.ts
@@ -93,7 +93,7 @@ export class DirectionMap {
     }
 
     private positionOf(stateChain: string[]): Position {
-        let pos: Position = { x: 0, y: 0 };
+        const pos: Position = { x: 0, y: 0 };
         for (const state of stateChain) {
             const curPos = this.positions.get(state) || ZeroPos;
             pos.x += curPos.x;
